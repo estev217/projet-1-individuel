25cc4f5886089d8648d813f43ada70ae7c27feb9
kevin <pininfarina47@gmail.com>
2019-09-18 17:13:03 +0200

    add js function for menu burger

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 function changePlace() {
     document.getElementById('formbutton').style.transform = "translate(850px, 0px)";
 }
+
+function hiddenMenu(){
+    var x = document.getElementById("mobile_menu"); 
+    console.log('ok')
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+    hiddenMenu();
