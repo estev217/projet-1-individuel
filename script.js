a521ffa1ddd2e9eddc070d28312b62a65045c0ca
harshbones <davranjar@hotmail.com>
2019-09-19 11:26:06 +0200

    adding toggle effect on texts in mobil

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+function toggleOne() {
+    var dots = document.getElementById("dots_1");
+    var moreText = document.getElementById("more_1");
+    var btnText = document.getElementById("btn_1");
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "&#x2193"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "&#x2191"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+  function toggleTwo() {
+    var dots = document.getElementById("dots_2");
+    var moreText = document.getElementById("more_2");
+    var btnText = document.getElementById("btn_2");
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "&#x2193"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "&#x2191"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+  function toggleThree() {
+    var dots = document.getElementById("dots_3");
+    var moreText = document.getElementById("more_3");
+    var btnText = document.getElementById("btn_3");
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "&#x2193"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "&#x2191"; 
+      moreText.style.display = "inline";
+    }
+}
 
 function changePlace() {
     document.getElementById('formbutton').style.transform = "translate(850px, 0px)";
