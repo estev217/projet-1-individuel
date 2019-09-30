e7a25089656bd9a5ae96eeca5fb9cbcac2f10b6a
Estevao Fernandes <estevao.fernandes@live.fr>
2019-09-30 09:20:10 +0200

    Creation of an array for section Notre Carte des Bières

--- array.php
+++ array.php
@@ -1,9 +1,10 @@
 <?php
-array ($bieres);
-$bieres [
-"images/bieres1.jpeg",
-"images/bieres2.jpeg",
-"images/bieres3.jpeg",
+$bieres = [
+0 => "images/bieres1.jpeg",
+1 => "images/bieres2.jpeg",
+2 => "images/bieres3.jpeg",
 ];
-echo $bieres;
+print "<img src=$bieres[0] alt=bières height='150px' width='150px'>";
+print "<img src=$bieres[1] alt=bières height='150px' width='150px'>";
+print "<img src=$bieres[2] alt=bières height='150px' width='150px'>";
 ?>
